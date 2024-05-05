4075c4ea9210265fbb58570ce0010b394b969fa3
Ritesh Shakya <riteshakya037@users.noreply.github.com>
2024-05-05 22:33:01 +0545

    Time: 38 ms (44.06%), Space: 16.5 MB (28.04%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -0,0 +1,18 @@
+class Solution:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        res = []
+
+        def dfs(pos, curr, target):
+            if len(curr) == k:
+                if target == 0:
+                    res.append(curr[:])
+                return
+
+            for i in range(pos + 1, 10):
+                if i <= target:
+                    curr.append(i)
+                    dfs(i, curr, target - i)
+                    curr.pop()
+
+        dfs(0, [], n)
+        return res
