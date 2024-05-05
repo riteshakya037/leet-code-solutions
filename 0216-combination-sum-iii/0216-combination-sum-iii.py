04f2b9eb5261b05237714ced45d481f6c8c07e14
Ritesh Shakya <riteshakya037@users.noreply.github.com>
2024-05-05 22:41:21 +0545

    Time: 39 ms (34.66%), Space: 16.5 MB (79.5%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.py
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -1,16 +1,18 @@
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
         res = []
-
-        def dfs(pos, curr, target):
-            if len(curr) == k:
-                if target == 0:
-                    res.append(curr[:])
-                return
-
-            for i in range(pos + 1, 10):
-                if i <= target:
-                    dfs(i, curr + [i], target - i)
-
-        dfs(0, [], n)
+        
+        # Iterate over all possible combinations of numbers using bitmask
+        for bitmask in range(1, 1 << 9):
+            curr = []
+            total_sum = 0
+            
+            for i in range(1, 10):
+                if bitmask & (1 << (i - 1)):  # Check if ith bit is set
+                    curr.append(i)
+                    total_sum += i
+            
+            if len(curr) == k and total_sum == n:
+                res.append(curr)
+        
         return res
