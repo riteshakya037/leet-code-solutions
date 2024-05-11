374ef0f874a0bdaf1562b7efcd4fb14633cb3e74
Ritesh Shakya <riteshakya037@users.noreply.github.com>
2024-05-11 23:21:43 +0545

    Time: 44 ms (7.68%), Space: 16.7 MB (9.91%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,6 +1,7 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        res = []
+        if not digits:
+            return []
         digitToChar = {
             "2": "abc",
             "3": "def",
@@ -11,16 +12,14 @@ class Solution:
             "8": "tuv",
             "9": "wxyz",
         }
+        res = [""]
 
-        def backtrack(i, currStr):
-            if len(currStr) == len(digits):
-                res.append(currStr)
-                return
 
-            for c in digitToChar[digits[i]]:
-                backtrack(i + 1, currStr + c)
-
-        if digits:
-            backtrack(0, "")
+        for digit in digits:
+            newRes = []
+            for combination in res:
+                for letter in digitToChar[digit]:
+                    newRes.append(combination + letter)
+            res = newRes
 
         return res
