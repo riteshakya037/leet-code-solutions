dc0b9d6a6cbbd2b8b41aed2263c33ceb701b980e
Ritesh Shakya <riteshakya037@users.noreply.github.com>
2024-05-25 17:43:34 +0545

    Time: 37 ms (50.18%), Space: 16.5 MB (92.31%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,20 +1,14 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        dp = { len(s): 1 }
-
-        def dfs(i):
-            if i in dp:
-                return dp[i]
+        dp = {len(s): 1}
+        for i in range(len(s) - 1, -1, -1):
             if s[i] == "0":
-                return 0
+                dp[i] = 0
+            else:
+                dp[i] = dp[i + 1]
 
-            res = dfs(i + 1)
             if i + 1 < len(s) and (
                 s[i] == "1" or s[i] == "2" and s[i + 1] in "0123456"
             ):
-                res += dfs(i + 2)
-
-            dp[i] = res
-            return res
-
-        return dfs(0)
+                dp[i] += dp[i + 2]
+        return dp[0]
