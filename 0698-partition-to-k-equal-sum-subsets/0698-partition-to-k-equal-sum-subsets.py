ea89498608d3d24cb0f2d0d5573042d09a84ef31
Ritesh Shakya <riteshakya037@users.noreply.github.com>
2024-06-09 00:36:36 +0545

    Time: 44 ms (96.76%), Space: 16.3 MB (99.62%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.py
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.py
@@ -1,33 +1,29 @@
 class Solution:
     def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
         n = len(nums)
-        target, rem = divmod(sum(nums), k)
+        eSum, rem = divmod(sum(nums), k)
         nums.sort(reverse=True)
-        if rem or nums[0] > target:
+        if rem or nums[0] > eSum:
             return False
 
-        def dfs(index, total, groups):
+        def dfs(index, target, groups):
             if groups == k - 1:
                 return True
             if index == n:
                 return False
 
             num = nums[index]
-            if num > total:
-                return dfs(index + 1, total, groups)
+            if num > target:
+                return dfs(index + 1, target, groups)
 
-            nums[index] = target + 1
-            if num == total:
-                return dfs(0, target, groups + 1)
+            nums[index] = eSum + 1
+            if num == target:
+                return dfs(0, eSum, groups + 1)
 
-            if dfs(index + 1, total - num, groups):
+            if dfs(index + 1, target - num, groups):
                 return True
 
             nums[index] = num
-            return dfs(index + 1, total, groups)
+            return dfs(index + 1, target, groups)
 
-        return dfs(0, target, 0)
-
-
-
-
+        return dfs(0, eSum, 0)
