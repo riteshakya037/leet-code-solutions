ca4bec69b9ed30b188e2b2539045074cf2967dd7
Ritesh Shakya <riteshakya037@users.noreply.github.com>
2024-06-09 00:02:53 +0545

    Time: 44 ms (96.76%), Space: 16.3 MB (99.62%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.py
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.py
@@ -1,37 +1,29 @@
 class Solution:
     def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
         n = len(nums)
+        eSum, rem = divmod(sum(nums), k)
         nums.sort(reverse=True)
-        total = sum(nums)
-        
-        if total % k:
+        if rem or nums[0] > eSum:
             return False
-        
-        target = total / k
-        used = [False] * n
-        
-        def dfs(index, total, k): 
-            
-            if k == 0:
+
+        def dfs(index, target, groups):
+            if groups == k - 1:
                 return True
-            
-            if total == 0:
-                return dfs(0, target, k - 1)
-            
-            for i in range(index, n):
-                
-                if i > 0 and not used[i - 1] and nums[i] == nums[i - 1]:
-                    continue
-                    
-                if used[i] or total - nums[i] < 0:
-                    continue
-                
-                used[i] = True
-                if dfs(i + 1, total - nums[i], k):
-                    return True
-                used[i] = False
-            return False
-        
-        return dfs(0, target, k)
+            if index == n:
+                return False
 
+            num = nums[index]
+            if num > target:
+                return dfs(index + 1, target, groups)
 
+            nums[index] = eSum + 1
+            if num == target:
+                return dfs(0, eSum, groups + 1)
+
+            if dfs(index + 1, target - num, groups):
+                return True
+
+            nums[index] = num
+            return dfs(index + 1, target, groups)
+
+        return dfs(0, eSum, 0)
