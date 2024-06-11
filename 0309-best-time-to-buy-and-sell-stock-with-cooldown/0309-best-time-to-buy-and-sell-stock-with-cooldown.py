c4f961d3d5c88fd9252c609f0e86834079156320
Ritesh Shakya <riteshakya037@users.noreply.github.com>
2024-06-11 07:31:38 +0545

    Time: 43 ms (67.93%), Space: 17.2 MB (55.91%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -2,23 +2,11 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         if len(prices) < 2:
             return 0
-        dp = {}
-
-        def dfs(i, buying):
-            if i >= len(prices):
-                return 0
-
-            if (i, buying) in dp:
-                return dp[(i, buying)]
-
-            cooldown = dfs(i + 1, buying)
-            if buying:
-                buy = dfs(i + 1, not buying) - prices[i]
-                dp[(i, buying)] = max(buy, cooldown)
-            else:
-                sell = dfs(i + 2, not buying) + prices[i]
-                dp[(i, buying)] = max(sell, cooldown)
-
-            return dp[(i, buying)]
-
-        return dfs(0, True)
+        dp = [[0] * 2 for _ in range(len(prices))]
+        dp[0][0] = -prices[0]
+        dp[1][1] = max(prices[1] - prices[0], 0)
+        dp[1][0] = max(-prices[1], -prices[0])
+        for i in range(2, len(dp)):
+            dp[i][0] = max(dp[i - 2][1] - prices[i], dp[i - 1][0])
+            dp[i][1] = max(dp[i - 1][0] + prices[i], dp[i - 1][1])
+        return dp[-1][1]
