4b3ee572854c25bbaf7b68b5b2f0e745b1bd8424
Ritesh Shakya <riteshakya037@users.noreply.github.com>
2024-06-16 06:42:56 +0545

    Time: 272 ms (66.62%), Space: 32.5 MB (28.83%) - LeetHub

--- 0148-sort-list/0148-sort-list.py
+++ 0148-sort-list/0148-sort-list.py
@@ -7,39 +7,48 @@ class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head or not head.next:
             return head
-        left = head
-        right = self.getMid(head)
-        temp = right.next
-        right.next = None
-        right = temp
+        # get size
+        size = 0
+        cur = head
+        while cur is not None:
+            cur = cur.next
+            size += 1
+        # split
+        step = 1
+        dummy = ListNode(None, head)
+        while step < size:
+            cur, pre = dummy.next, dummy
+            while cur is not None:
+                # split left half
+                l = r = cur
+                i = 1
+                while i < step and r is not None:
+                    r = r.next
+                    i += 1
+                if r is not None:
+                    tmp = r.next
+                    r.next = None
+                    r = tmp
 
-        left = self.sortList(left)
-        right = self.sortList(right)
+                # split right half
+                cur = r
+                i = 1
+                while i < step and cur is not None:
+                    cur = cur.next
+                    i += 1
+                if cur is not None:
+                    tmp = cur.next
+                    cur.next = None
+                    cur = tmp
 
-        return self.merge(left, right)
-
-    def getMid(self, head: ListNode):
-        slow, fast = head, head.next
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-        return slow
-
-    def merge(self, left: ListNode, right: ListNode):
-        tail = dummy = ListNode()
-
-        while left and right:
-            if left.val < right.val:
-                tail.next = left
-                left = left.next
-            else:
-                tail.next = right
-                right = right.next
-            tail = tail.next
-
-        if left:
-            tail.next = left
-        if right:
-            tail.next = right
-
+                # merge
+                while l is not None and r is not None:
+                    if l.val <= r.val:
+                        pre.next, l = l, l.next
+                    else:
+                        pre.next, r = r, r.next
+                    pre = pre.next
+                pre.next = l if l is not None else r
+                while (nxt := pre.next) is not None: pre = nxt
+            step <<= 1
         return dummy.next
