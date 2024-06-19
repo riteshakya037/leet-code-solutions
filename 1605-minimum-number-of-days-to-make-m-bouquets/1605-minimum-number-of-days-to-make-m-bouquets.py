2990cf11441c1b61672a6a2454ef33fbc1777934
Ritesh Shakya <riteshakya037@users.noreply.github.com>
2024-06-19 06:56:33 +0545

    Time: 690 ms (99.93%), Space: 30.3 MB (75.28%) - LeetHub

--- 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.py
+++ 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.py
@@ -1,34 +1,28 @@
 class Solution:
     def minDays(self, bloomDay: List[int], m: int, k: int) -> int:
+        def isValid(x):
+            total = 0
+            currcount = 0
+            for b in bloomDay:
+                if x >= b:
+                    currcount += 1
+                else:
+                    total += currcount // k
+                    if total >= m:
+                        return True
+                    currcount = 0
+            total += currcount // k
+            if total >= m:
+                return True
+
         if m * k > len(bloomDay):
             return -1
-
-        min_value, max_value = min(bloomDay), max(bloomDay)
-        answer = max_value
-
-        while min_value <= max_value:
-            mid = (min_value + max_value) // 2
-
-            count, bouquets, adjacent = 0, 0, False
-            for i in range(len(bloomDay)):
-                if bloomDay[i] <= mid:
-                    if count == 0:
-                        adjacent = True
-                        count += 1
-                    elif adjacent:
-                        count += 1
-                else:
-                    count = 0
-                    adjacent = False
-
-                if count == k:
-                    bouquets += 1
-                    count, adjacent = 0, False
-
-            if bouquets >= m:
-                answer = min(answer, mid)
-                max_value = mid - 1
+        left = min(bloomDay)
+        right = max(bloomDay)
+        while left < right:
+            mid = left + (right - left) // 2
+            if isValid(mid):
+                right = mid
             else:
-                min_value = mid + 1
-
-        return answer
+                left = mid + 1
+        return left
