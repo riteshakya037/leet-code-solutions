642fdf6456980b53828c592ff16f2ec337f3b420
Ritesh Shakya <riteshakya037@users.noreply.github.com>
2024-06-21 08:26:36 +0545

    Time: 224 ms (27.43%), Space: 19 MB (14.93%) - LeetHub

--- 1138-grumpy-bookstore-owner/1138-grumpy-bookstore-owner.py
+++ 1138-grumpy-bookstore-owner/1138-grumpy-bookstore-owner.py
@@ -1,23 +1,34 @@
 class Solution:
-    def maxSatisfied(self, customers: List[int], grumpy: List[int], minutes: int) -> int:
-        total_sum = 0
-        for i in range(len(customers)):
-            if grumpy[i] == 0:
-                total_sum += customers[i]
+    def maxSatisfied(
+        self, customers: List[int], grumpy: List[int], minutes: int
+    ) -> int:
+        n = len(customers)
+        unrealized_customers = 0
 
-        satisfaction = total_sum
-        start = 0
-        end = start + minutes - 1
+        # Calculate initial number of unrealized customers in first 'minutes' window
+        for i in range(minutes):
+            unrealized_customers += customers[i] * grumpy[i]
 
-        while end < len(customers):
-            temp_sum = 0
-            for i in range(start, end + 1):
-                if grumpy[i] == 1:
-                    temp_sum += customers[i]
+        max_unrealized_customers = unrealized_customers
 
-            satisfaction = max(satisfaction, total_sum + temp_sum)
+        # Slide the 'minutes' window across the rest of the customers array
+        for i in range(minutes, n):
+            # Add current minute's unsatisfied customers if the owner is grumpy
+            # and remove the customers that are out of the current window
+            unrealized_customers += customers[i] * grumpy[i]
+            unrealized_customers -= customers[i - minutes] * grumpy[i - minutes]
 
-            start += 1
-            end += 1
+            # Update the maximum unrealized customers
+            max_unrealized_customers = max(
+                max_unrealized_customers, unrealized_customers
+            )
 
-        return satisfaction
+        # Start with maximum possible satisfied customers due to secret technique
+        total_customers = max_unrealized_customers
+
+        # Add the satisfied customers during non-grumpy minutes
+        for i in range(n):
+            total_customers += customers[i] * (1 - grumpy[i])
+
+        # Return the maximum number of satisfied customers
+        return total_customers
