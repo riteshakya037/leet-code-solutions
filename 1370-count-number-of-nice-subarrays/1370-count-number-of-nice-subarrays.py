572e0d8752503694bcccdc2056b59ad6ea62716c
Ritesh Shakya <riteshakya037@users.noreply.github.com>
2024-06-22 08:05:11 +0545

    Time: 559 ms (96.14%), Space: 23.5 MB (67.27%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.py
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.py
@@ -1,15 +1,17 @@
 class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
-        return self.atMost(nums, k) - self.atMost(nums, k - 1)
-
-    def atMost(self, nums: List[int], k: int) -> int:
-        window_size, subarrays, start = 0, 0, 0
-        for end in range(len(nums)):
-            window_size += nums[end] % 2
-            # Find the first index start where the window has exactly k odd elements.
-            while window_size > k:
-                window_size -= nums[start] % 2
+        subarrays = 0
+        start = 0
+        qsize = 0
+        initial_gap = 0
+        for r in range(len(nums)):
+            if nums[r] % 2:
+                qsize += 1
+                initial_gap = 0
+            while qsize == k:
+                initial_gap += 1
+                if nums[start] % 2:
+                    qsize -= 1
                 start += 1
-            # Increment number of subarrays with end - start + 1.
-            subarrays += end - start + 1
+            subarrays += initial_gap
         return subarrays
