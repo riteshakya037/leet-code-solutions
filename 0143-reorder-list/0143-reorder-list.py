b37acf6f7d2e029b7ecd191297bbf3ee4ad053eb
Ritesh Shakya <riteshakya037@users.noreply.github.com>
2024-06-23 06:45:12 +0545

    Time: 53 ms (70.59%), Space: 24.6 MB (72.72%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -5,22 +5,24 @@
 #         self.next = next
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
-        queue = collections.deque()
-        currentNode = head.next
+        slow, fast = head, head.next
 
-        while currentNode:
-            queue.append(currentNode)
-            currentNode = currentNode.next
-        
-        currentNode = head
-        while len(queue) > 1: 
-            currentNode.next = queue.pop()
-            currentNode = currentNode.next
-            currentNode.next = queue.popleft()
-            currentNode = currentNode.next
-        
-        if queue:
-            currentNode.next = queue.pop()
-            currentNode = currentNode.next
-        
-        currentNode.next = None
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        second = slow.next
+        prev = slow.next = None
+
+        while second:
+            tmp = second.next
+            second.next = prev
+            prev = second
+            second = tmp
+
+        first, second = head, prev
+        while second:
+            tmp1, tmp2 = first.next, second.next
+            first.next = second
+            second.next = tmp1
+            first, second = tmp1, tmp2
