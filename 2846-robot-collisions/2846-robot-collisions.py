ddeb9c6d45ebd86d3a0857f8599bddc20622d4dc
Ritesh Shakya <riteshakya037@users.noreply.github.com>
2024-07-13 09:37:02 +0545

    Time: 945 ms (78.85%), Space: 35.3 MB (75.96%) - LeetHub

--- 2846-robot-collisions/2846-robot-collisions.py
+++ 2846-robot-collisions/2846-robot-collisions.py
@@ -1,51 +1,19 @@
 class Solution:
-    class Robot:
-        def __init__(self, position: int, health: int, direction: str, index: int):
-            self.position = position
-            self.health = health
-            self.direction = direction
-            self.index = index
-
     def survivedRobotsHealths(
         self, positions: List[int], healths: List[int], directions: str
     ) -> List[int]:
-        n = len(positions)
-        vals = [
-            self.Robot(positions[i], healths[i], directions[i], i) for i in range(n)
-        ]
-        vals.sort(key=lambda x: x.position)
-
-        stack = []
-        for robot in vals:
-            if robot.direction == "R":
-                stack.append(robot)
-                continue
-
-            gone = False
-            while (
-                stack
-                and stack[-1].health <= robot.health
-                and stack[-1].direction == "R"
-            ):
-                if stack[-1].health == robot.health:
-                    stack.pop()
-                    gone = True
-                    break
-                robot.health -= 1
-                stack.pop()
-
-            if (
-                not gone
-                and stack
-                and stack[-1].direction == "R"
-                and stack[-1].health > robot.health
-            ):
-                stack[-1].health -= 1
-                gone = True
-
-            if not gone:
-                stack.append(robot)
-
-        stack.sort(key=lambda x: x.index)
-
-        return [robot.health for robot in stack]
+        left, right = [], []
+        for i in sorted(range(len(positions)), key=lambda i: positions[i]):
+            if directions[i] == "R":
+                right.append(i)
+            else:
+                while right and healths[right[-1]] < healths[i]:
+                    right.pop()
+                    healths[i] -= 1
+                if not right:
+                    left.append(i)
+                elif healths[right[-1]] == healths[i]:
+                    right.pop()
+                else:
+                    healths[right[-1]] -= 1
+        return [healths[i] for i in sorted(left + right)]
