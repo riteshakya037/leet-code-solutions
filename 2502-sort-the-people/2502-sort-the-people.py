f26d4a3f7fcbe57dc2c08e902f2791dc59dda540
Ritesh Shakya <riteshakya037@users.noreply.github.com>
2024-07-22 07:50:59 +0545

    Time: 101 ms (75.15%), Space: 17.1 MB (45.1%) - LeetHub

--- 2502-sort-the-people/2502-sort-the-people.py
+++ 2502-sort-the-people/2502-sort-the-people.py
@@ -1,13 +1,8 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        number_of_people = len(names)
+        d = {}
+        descend = [""] * len(names)
+        d = dict(zip(heights, names))
 
-        # Create a dictionary to store height-name pairs
-        height_to_name_map = dict(zip(heights, names))
-
-        sorted_heights = sorted(heights, reverse=True)
-
-        # Create a list of sorted names based on descending heights
-        sorted_names = [height_to_name_map[height] for height in sorted_heights]
-
-        return sorted_names  
+        h = sorted(heights)
+        return [d[height] for height in h[::-1]]
