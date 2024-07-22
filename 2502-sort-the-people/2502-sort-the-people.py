77093e389bd3944d54ef0153f0597410b8642671
Ritesh Shakya <riteshakya037@users.noreply.github.com>
2024-07-22 07:51:44 +0545

    Time: 103 ms (65.9%), Space: 17 MB (84.16%) - LeetHub

--- 2502-sort-the-people/2502-sort-the-people.py
+++ 2502-sort-the-people/2502-sort-the-people.py
@@ -1,7 +1,6 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
         d = {}
-        descend = [""] * len(names)
         d = dict(zip(heights, names))
         h = sorted(heights, reverse=True)
         return [d[height] for height in h]
